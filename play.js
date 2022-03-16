d13f2cfd185e8e54ed915d8d6cf5997604812fbf
Rabhas <rabhas.vashisht@gmail.com>
2022-03-16 21:52:48 +0000

    exported setinput and handleuser from input and required it in play.js

--- play.js
+++ play.js
@@ -1,23 +1,7 @@
 const net = require('net');
+const { setupInput } = require('./input');
 // establishes a connection with the game server
 const { connect } = require('./client')
 console.log("Connecting ...");
 connect();
 
-const handleUserInput = function(key) {
-  
-  if (key === '\u0003') {
-    process.exit();
-  }
-  
-};
-
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.on('data', key => handleUserInput(key));
-  stdin.resume();
-  return stdin;
-};
