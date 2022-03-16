5c024b45d480252122c8a7d6cc8aa5ab16a1f4fa
Rabhas <rabhas.vashisht@gmail.com>
2022-03-16 21:37:25 +0000

    setup stdin by creating setinput and handleUserInput functions

--- play.js
+++ play.js
@@ -1,6 +1,23 @@
 const net = require('net');
 // establishes a connection with the game server
 const { connect } = require('./client')
-
 console.log("Connecting ...");
 connect();
+
+const handleUserInput = function(key) {
+  
+  if (key === '\u0003') {
+    process.exit();
+  }
+  
+};
+
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.on('data', key => handleUserInput(key));
+  stdin.resume();
+  return stdin;
+};
